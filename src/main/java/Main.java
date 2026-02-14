4b5a94b3a15cd8c091a3b98457d103273dd71502
Komeshan <komeshan002@gmail.com>
2026-02-14 20:17:07 +0530

    Day 2 - Database setup + Singleton DatabaseConnection created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import util.DatabaseConnection;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ocean View Resort Reservation System");
+        Connection conn = DatabaseConnection.getInstance();
+        if (conn != null) {
+            System.out.println("Test successful: Database is connected!");
+        }
     }
 }
